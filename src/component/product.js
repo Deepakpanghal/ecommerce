f37f6a643078270371cffe9f6463cca02704d4cb
Deepakpanghal <deepakpanghal52@gmail.com>
2022-03-13 20:06:58 +0530

    Deploy my app

---
+++ src/component/product.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    title: "One Bowl Chocolate Cake",
+    price: "500₹",
+    img: "https://images.pexels.com/photos/1854652/pexels-photo-1854652.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Cup Cake",
+    price: "800₹",
+    img: "https://images.pexels.com/photos/3081657/pexels-photo-3081657.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Birthday Special",
+    price: "500₹",
+    img: "https://images.pexels.com/photos/3038302/pexels-photo-3038302.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "cream chocolate",
+    price: "300₹",
+    img: "https://images.pexels.com/photos/1998633/pexels-photo-1998633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    quantity: 1,
+  },
+];
